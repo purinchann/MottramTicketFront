3d90f7e8c1e1e1de9612c5d13eeb14976c78dbae
中村　鷹祐 <naka777you@gmail.com>
2019-06-07 03:50:29 +0900

    index.htmlだけメタタグを記述

--- src/app/component/signup/signup.component.ts
+++ src/app/component/signup/signup.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/service/auth.service';
+//import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-signup',
@@ -16,6 +17,7 @@ export class SignupComponent implements OnInit {
     private router: Router,
     private fb: FormBuilder,
     private auth: AuthService
+    //private meta: Meta
   ) { 
     this.auth.user.subscribe(user => {
       if (user !== null) {
@@ -29,6 +31,19 @@ export class SignupComponent implements OnInit {
       'email': ['', [Validators.required, Validators.email]],
       'password': ['', [Validators.required]]
     });
+
+    // this.meta.addTags([
+    //   { name: 'viewport', content: 'width=device-width,initial-scale=1'},
+    //   { name: 'description', content: '【モッチャムチケット】はあなたの代わりにモッチャムの行列に並び、あなたが飲みたいタピオカを手に入れて、あなたにお渡しするタピオカ整理券サービスです。'},
+    //   { name: 'keywords', content: 'モッチャム,整理券,モッチャム 時間,モッチャム 待ち,モッチャム 並ぶ'},
+    //   { name: 'og:locale', content: 'ja_JP'},
+    //   { name: 'og:url', content: '/login' },
+    //   { name: 'og:title', content: 'これからのモッチャムはいつでもお手軽にゲットしよう！' },
+    //   { name: 'og:description', content: 'これからのモッチャムはいつでもお手軽にゲットしよう！' },
+    //   { name: 'twitter:card', content: 'summary' },
+    //   { name: 'twitter:title', content: 'モッチャムチケット'},
+    //   { name: 'twitter:description', content: 'モッチャムチケット ログイン画面'}
+    // ]);
   }
 
   signup() {
